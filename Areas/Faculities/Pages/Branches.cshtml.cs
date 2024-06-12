6e4f12fdef834fc3a0826ef9b95e6c5aa1244d48
hmquyuef <quyhm@uef.edu.vn>
2024-06-12 16:34:18 +0700

    Add project files.

---
+++ Areas/Faculities/Pages/Branches.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudentPortal.Areas.Faculities.Pages
+{
+    public class BranchesModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Areas/Faculities/Pages/Majors.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudentPortal.Areas.Faculities.Pages
+{
+    public class MajorsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
