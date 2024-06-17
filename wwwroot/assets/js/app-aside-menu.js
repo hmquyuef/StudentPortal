cc455b0867ea50776ad33ada84a9a5ae40925f9d
hmquyuef <quyhm@uef.edu.vn>
2024-06-17 16:42:27 +0700

    edit settings

--- wwwroot/assets/js/app-aside-menu.js
+++ wwwroot/assets/js/app-aside-menu.js
@@ -1,15 +1,33 @@
 ﻿$(document).ready(function () {
-    renderAsideMenu()
+    renderAsideMenu();
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "5000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    };
 });
 function renderAsideMenu() {
+    $("#loadUrl").empty();
     $.ajax({
         url: 'http://192.168.98.60:8080/api/settings',
         type: 'GET',
         success: function (result) {
-            console.log("MENU: ", result)
+            console.log("MENU URL: ", result.data.items)
             if (result.data.items.length > 0) {
                 result.data.items.map((item, index) => {
-                    if (item.childrensMenus.length > 0) {
+                    if (item.children.length > 0) {
                         //get current page url
                         var currentPage = window.location.pathname;
                         console.log(currentPage)
@@ -20,12 +38,12 @@ function renderAsideMenu() {
                                     <div class="text-truncate">${item.text}</div>
                                 </a>
                             </li>`);
-                        item.childrensMenus.map((child, index) => {
+                        item.children.map((child, index) => {
                             $("#" + item.id).append(`
                                 <ul class="menu-sub">
                                     <li class="menu-item ${currentPage === (item.url + child.url) ? 'active' : ''}">
                                         <a href="${item.url + child.url}" class="menu-link">
-                                            <div class="text-truncate">${child.url}</div>
+                                            <div class="text-truncate">${child.text}</div>
                                         </a>
                                     </li>
                                 </ul>`);
